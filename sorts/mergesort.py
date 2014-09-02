265c2e32df4feb17d44aa2ff8ab02fb9dd481742
Bradley Moore <BradleyMooreTX@gmail.com>
2014-09-02 09:25:07 -0600

    clarify thinking by beginning pseudocode outline

---
+++ sorts/mergesort.py
@@ -0,0 +1,21 @@
+'''
+split func
+accept input
+if len <= 1
+	return input
+else 
+	mid = len unsorted / 2
+	left = 0
+	right = mid
+
+sort func
+while len left > 0 and len right > 0
+	if len left < 1
+		append right(all)
+	else if len right < 1
+		append left(all)
+	else
+		if left < right
+			append left
+		else append right
+'''
