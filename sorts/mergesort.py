d2af1ece3552f7ec9fd2a7304a8e648aa591f41d
BradleyMoore <BradleyMooreTX@gmail.com>
2014-09-08 20:49:24 +0000

    have working mergesort algorithm

--- sorts/mergesort.py
+++ sorts/mergesort.py
@@ -8,12 +8,16 @@ def mergesort(unsorted):
     left = unsorted[:mid]
     right = unsorted[mid:]
     sorted = []
+    print str(left) + ' <-- left'
+    print str(right) + ' <-- right'
 
     # recursively split the lists down to lengths of 1
     if len(left) > 1:
-        mergesort(left)
-    elif len(right) > 1:
-        mergesort(right)
+        print str(left) + ' <-- merge left'
+        left = mergesort(left)
+    if len(right) > 1:
+        print str(right) + ' <-- merge right'
+        right = mergesort(right)
 
     i = 0
     j = 0
@@ -23,12 +27,18 @@ def mergesort(unsorted):
     else:
         iter_length = len(left)
 
-    for k in xrange(iter_length):
-        # if one side gets finished just append the rest of the other sorted side
+    for k in xrange(len(unsorted)):
+        # if one side gets finished just extend the rest of the other sorted side
         if i == len(left):
-            sorted.append(right[j:])
+            sorted.extend(right[j:])
+            j = j + 1
+            print str(j) + ' <-- j'
+            break
         elif j == len(right):
-            sorted.append(left[i:])
+            sorted.extend(left[i:])
+            i = i + 1
+            print str(i) + ' <-- i'
+            break
 
         # append lesser of the 2 options
         if left[i] < right[j]:
@@ -38,10 +48,12 @@ def mergesort(unsorted):
             sorted.append(right[j])
             j = j + 1
 
-    print sorted
+    print str(sorted) + ' <-- sorted'
+    return sorted
 
 
-while __name__ == '__main__':
-    list = [1,245,356,467,689,678,3456,234,234,234,346,56,231,212,13,24,345,456,68,76,345,34,134]
-    mergesort(list)
+if __name__ == '__main__':
+    list = [4,7,3,2,57,4,345,768,89,345,234,56,768,789,768,4,23,45,568,89,0,6,234,2345,768,79,0,789,356,23,1,346,578,76,4,235,346,68,247,383,3,568,58,56,7,3,4,36,76,54]
+    sort = mergesort(list)
+    print str(sort) + ' <-- sort'
 
