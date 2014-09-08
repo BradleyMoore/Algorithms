7046953d77f6c469879682e10b91e1365e275744
BradleyMoore <BradleyMooreTX@gmail.com>
2014-09-08 15:10:30 +0000

    remove initial thinking outline

--- sorts/mergesort.py
+++ sorts/mergesort.py
@@ -1,29 +1,3 @@
-'''
-mergesort func
-accept input
-split
-sort
-
-split func
-if len <= 1
-	return input
-else 
-	mid = len unsorted / 2
-	left = 0
-	right = mid
-
-sort func
-while len left > 0 or len right > 0
-	if len left < 1
-		append right(all)
-	else if len right < 1
-		append left(all)
-	else
-		if left < right
-			append left
-		else append right
-'''
-
 def mergesort (unsorted):
 	if len(unsorted <= 1):
 		return unsorted
