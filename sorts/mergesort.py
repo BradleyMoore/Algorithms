a18da87af22d4da8c603d1cc3602a9b6ebc52325
BradleyMoore <BradleyMooreTX@gmail.com>
2014-09-08 15:23:40 +0000

    add some much needed comments

--- sorts/mergesort.py
+++ sorts/mergesort.py
@@ -1,12 +1,14 @@
 def mergesort (unsorted):
+    # if length of lilst is <= 1 it is already sorted
 	if len(unsorted <= 1):
 		return unsorted
 	
 	mid = int(len(unsorted) / 2)
 	left = unsorted[:mid]
 	right = unsorted[mid:]
-    sorted = [0 for i in xrange(unsorted)]
+    sorted = []
 
+    # recursively split the lists down to lengths of 1
     if len(left) > 1:
         mergesort(left)
     else if len(right) > 1:
@@ -19,6 +21,14 @@ def mergesort (unsorted):
         iter_length = len(left)
     else:
         iter_length = len(right)
-
     
+    for k in xrange(unsorted):
+        # if one side gets finished just append the rest of the other sorted side
+        if i == len(left):
+           sorted.append(right[j:]
+        else if j == len(right):
+            sorted.append(left[i:]
+
+
+
 
