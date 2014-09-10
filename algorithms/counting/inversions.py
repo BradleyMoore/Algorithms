1104099d47505373904d452acb25d96b99add541
BradleyMoore <BradleyMooreTX@gmail.com>
2014-09-10 16:40:53 +0000

    begin inversions function and call mergesort on the 2 lists

--- algorithms/counting/inversions.py
+++ algorithms/counting/inversions.py
@@ -1,2 +1,20 @@
-from ..sorting import mergesort
+from ..sorting.mergesort import mergesort
+
+
+def inversions(unsorted1, unsorted2):
+    sorted1 = []
+    sorted2 = []
+
+    sorted1 = mergesort(unsorted1)
+    sorted2 = mergesort(unsorted2)
+
+
+
+
+
+
+
+
+
+
 
