d8e4293bf015888a5494f0957f29b735305fcc41
BradleyMoore <BradleyMooreTX@gmail.com>
2014-09-10 19:03:22 +0000

    refactor into separate functions for readability and easier reuse

--- algorithms/sorting/mergesort.py
+++ algorithms/sorting/mergesort.py
@@ -2,11 +2,20 @@ import sys
 
 
 def mergesort(unsorted):
+    sorted = []
 
     # if length of list is <= 1 it is already sorted
     if len(unsorted) <= 1:
         return unsorted
 
+    left, right = split_list(unsorted)
+
+    sorted = merge(left, right)
+
+    return sorted
+
+
+def split_list(unsorted):
     mid = len(unsorted) / 2
     left = unsorted[:mid]
     right = unsorted[mid:]
@@ -18,23 +27,21 @@ def mergesort(unsorted):
     if len(right) > 1:
         right = mergesort(right)
 
+    return (left, right)
+
+
+def merge(left, right):
+    sorted = []
+
     i = 0
     j = 0
-    iter_length = 0
-    if len(left) > len(right):
-        iter_length = len(right)
-    else:
-        iter_length = len(left)
-
-    for k in xrange(len(unsorted)):
+    for k in xrange(len(left) + len(right)):
         # if one side gets finished just extend the rest of the other sorted side
         if i == len(left):
             sorted.extend(right[j:])
-            j = j + 1
             break
         elif j == len(right):
             sorted.extend(left[i:])
-            i = i + 1
             break
 
         # append lesser of the 2 options
@@ -51,6 +58,7 @@ def mergesort(unsorted):
 if __name__ == '__main__':
     unsorted = [3,345,456,7,879,970,7,4,23,123,45,467,578,78,6,4,324,145,345,3456,567,5768,6589,69,69]
     sort = mergesort(unsorted)
+
     print '%r <-- unsorted' % unsorted
     print '%r <-- sorted' % sort
 
