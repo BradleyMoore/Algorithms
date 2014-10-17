672952c804da2fe5133219b6656cc4d0bf95cbe0
Bradley Moore <bradleymooretx@gmail.com>
2014-10-17 20:29:15 +0000

    infinite loop; sad day

--- algorithms/sorting/quicksort.py
+++ algorithms/sorting/quicksort.py
@@ -4,15 +4,15 @@ from random import randint
 def quicksort(unsorted, start=0, end=0):
     if len(unsorted) <= 1:
         return unsorted
-    
+
     sorted = sort(unsorted, start, end)
-    
+
     return sorted
-    
-    
+
+
 def choose_pivot(start, end):
     pivot = randint(start, end-1)
-    
+
     return pivot
 
 
@@ -26,29 +26,27 @@ def sort(unsorted, pivot, start=0, end=0):
     for i in xrange(start, end):
         if unsorted[i] < unsorted[pivot]:
             unsorted.insert(last_less, unsorted[i])
-            unsorted.remove(i)
+            unsorted.remove(unsorted[i])
             if i > pivot:
                 pivot = pivot + 1
             last_less = last_less + 1
             continue
         elif unsorted[i] > unsorted[pivot]:
             unsorted.insert(end, unsorted[i])
-            unsorted.remove(i)
+            unsorted.remove(unsorted[i])
             if i < pivot:
                 pivot = pivot - 1
             continue
         else:
             continue
 
-    unsorted[i], unsorted[pivot] = unsorted[pivot], unsorted[i]    
+    unsorted[i], unsorted[pivot] = unsorted[pivot], unsorted[i]
 
     partial_sort = quicksort(unsorted, 0, pivot-1)
     partial_sort = quicksort(partial_sort, pivot+1, len(unsorted))
 
-    for i in xrange(len(unsorted)-1):
-        if partial_sort[i] > partial_sort[i+1]:
-            sorted = unsorted
-            return sorted
+    sorted = partial_sort
+    return sorted
 
 
 if __name__ == '__main__':
