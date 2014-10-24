36a52ba453efab888b22daceed515ae85d5108cb
BradleyMoore <BradleyMooreTX@gmail.com>
2014-10-24 19:29:13 +0000

    remove name == main used for testing

--- algorithms/sorting/quicksort.py
+++ algorithms/sorting/quicksort.py
@@ -1,31 +1,49 @@
-import random
+from random import randint
 
 
-def quicksort(unsorted):
-    """Input unsorted list and return either list if len < 2 or return sorted list"""
-    if len(unsorted)<2:
-        return unsorted
+def partition(unsorted, start, end):
+    """Return final resting place of pivot; RIP sweet pivot."""
+    pivot_index = choose_pivot(start, end)
+    pivot = unsorted[pivot_index]
 
-    pivot = choose_pivot(unsorted)
-    left, equals, right = partition(unsorted, pivot)
+    # move pivot to beginning for logic
+    unsorted[start], unsorted[pivot_index] = unsorted[pivot_index], unsorted[start]
+    i = start + 1
+    j = start + 1
 
-    sorted = quicksort(left) + equals + quicksort(right)
-    return sorted
+    # move elements less than pivot to the left so the pivot can be swapped later
+    while j < end:
+        if unsorted[j] < pivot:
+            unsorted[j], unsorted[i] = unsorted[i], unsorted[j]
+            i = i + 1
+        j = j + 1
+
+    # swat pivot and last number < it; pivot is now in it's proper place
+    unsorted[start], unsorted[i - 1] = unsorted[i - 1], unsorted[start]
+    return i - 1
 
 
-def choose_pivot(unsorted):
-    """Input unsorted list and return a random item from it"""
-    pivot = random.choice(unsorted)
+def choose_pivot(start, end):
+    """Input start and end locations of list and return a random number between the 2."""
+    pivot = randint(start, end)
     return pivot
 
 
-def partition(unsorted, pivot):
-    """Input unsorted list and the randomized element and return 3 quicksorted lists"""
-    left = [i for i in unsorted if i < pivot]
-    equals = [i for i in unsorted if i == pivot]
-    right = [i for i in unsorted if i > pivot]
+def quicksort(unsorted, start=0, end=None):
+    """Return sorted list."""
+    if end == None:
+        end = len(unsorted) - 1
 
-    return left, equals, right
+    # return sorted list
+    if end - start < 1:
+        return unsorted
+
+    pivot_index = partition(unsorted, start, end)
+
+    # recursively sort everything and return it
+    quicksort(unsorted, start, pivot_index - 1)
+    quicksort(unsorted, pivot_index + 1, end)
+
 
 
 if __name__ == '__main__':
@@ -34,4 +52,3 @@ if __name__ == '__main__':
 
     print '%r <-- unsorted' % unsorted
     print '%r <-- sorted' % sorted
-
