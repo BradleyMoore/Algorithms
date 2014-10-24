84b932df5520901645c6d999abddea1191654a34
BradleyMoore <BradleyMooreTX@gmail.com>
2014-10-24 18:31:21 +0000

    create skeleton of a proper in place quicksort

---
+++ algorithms/sorting/quicksort_ip.py
@@ -0,0 +1,21 @@
+from random import randint
+
+
+def partition(unsorted, start, end, pivot):
+    pass
+
+
+def choose_pivot(start, end):
+    pass
+
+
+def quicksort(unsorted, start=0, end=None):
+    pass
+
+
+if __name__ == '__main__':
+    unsorted = [3,345,456,7,879,970,7,4,23,123,45,467,578,78,6,4,324,145,345,3456,567,5768,6589,69,69]
+    sorted = quicksort(unsorted)
+
+    print '%r <-- unsorted' % unsorted
+    print '%r <-- sorted' % sorted
