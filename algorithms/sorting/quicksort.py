a9a2e6276af48549bd46d9c0493f7cbfb77c49c7
BradleyMoore <BradleyMooreTX@gmail.com>
2014-10-26 22:16:43 +0000

    refactor to work now when called instead of coding in a list

--- algorithms/sorting/quicksort.py
+++ algorithms/sorting/quicksort.py
@@ -43,12 +43,3 @@ def quicksort(unsorted, start=0, end=None):
     # recursively sort everything and return it
     quicksort(unsorted, start, pivot_index - 1)
     quicksort(unsorted, pivot_index + 1, end)
-
-
-
-if __name__ == '__main__':
-    unsorted = [3,345,456,7,879,970,7,4,23,123,45,467,578,78,6,4,324,145,345,3456,567,5768,6589,69,69]
-    sorted = quicksort(unsorted)
-
-    print '%r <-- unsorted' % unsorted
-    print '%r <-- sorted' % sorted
